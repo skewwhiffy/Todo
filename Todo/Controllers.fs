a7167d55af6f123fe9c1ca604bd98bcad00df5cb
Kenny Hung <kenny.hung@just-eat.com>
2016-11-20 19:24:57 +0000

    Angular, and MVC framework in F#

---
+++ Todo/Controllers.fs
@@ -0,0 +1,18 @@
+﻿namespace Todo
+
+open System.Web.Mvc
+open System.Web.Http
+
+type HomeController() =
+    inherit Controller()
+
+    member this.Index() =
+        base.View()
+
+type Todo = { Title:string }
+
+type TodoController() =
+    inherit ApiController()
+
+    member this.Get() =
+        [{Title="todo"}; {Title="todo2"}]
---
+++ Todo/Resources/Scripts/index.js
@@ -0,0 +1,10 @@
+﻿var app = angular.module("Todo", []);
+app.controller("homeController",
+    function($scope, $http) {
+        "use strict";
+
+        $http.get("/api/todo")
+            .then(function(response) {
+                $scope.list = response.data;
+            });
+    });
